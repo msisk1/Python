0d8b4a39e180d0f86210151698e1baf0f66d3528
msisk1 <msisk1@nd.edu>
2017-10-26 15:09:34 -0400

    Rollins cluster analysis ver 02

--- Rollins_PointsInClusters.py
+++ Rollins_PointsInClusters.py
@@ -18,9 +18,9 @@ clusterTable = "E:\\GISWork_2\\Rollins_poximity\\GIS\\Wave 3\\clusterTable.shp"
 outTable = "E:\\GISWork_2\\Rollins_poximity\\GIS\\Wave 3\\putTbale.csv"
 
 
-clisters3 = "E:\\GISWork_2\\Rollins_poximity\\GIS\\Wave 3\\clisters3.shp"
+clusters3 = "E:\\GISWork_2\\Rollins_poximity\\GIS\\Wave 3\\clisters3.shp"
 geo_2006_SpatialJoin = "E:\\GISWork_2\\Rollins_poximity\\GIS\\Wave 3\\geo_2006_SpatialJoin.shp"
-geo_2006_SpatialJoin__3_ = geo_2006_SpatialJoin
+
 
 
 arcpy.env.overwriteOutput = True
@@ -50,18 +50,18 @@ deleteAllFieldsBut(clusterTable, ["FID", "Shape","Join_Count","ClusterID"])
 #arcpy.DeleteField_management(clusterTable, "TARGET_FID;COUNT_FID;ObjectID_1;FacilityID;Name;FromBreak;ToBreak")
 
 # Process: Adding on a number of points
-arcpy.SpatialJoin_analysis(clusterTable, points, clisters3, "JOIN_ONE_TO_ONE", "KEEP_ALL", "Join_Count \"Join_Count\" true true false 0 Long 0 0 ,First,#,C:\\Users\\msisk1\\Documents\\ArcGIS\\Default.gdb\\clusterTable,Join_Count,-1,-1;ObjectID \"ObjectID\" true true false 9 Long 0 9 ,First,#,C:\\Users\\msisk1\\Documents\\ArcGIS\\Default.gdb\\clusterTable,ObjectID,-1,-1;ClusterID \"ClusterID\" true true false 0 Short 0 0 ,First,#,C:\\Users\\msisk1\\Documents\\ArcGIS\\Default.gdb\\clusterTable,ClusterID,-1,-1;Status \"Status\" true true false 1 Text 0 0 ,First,#,E:\\GISWork_2\\Rollins_poximity\\GIS\\Wave 3\\Geocoding points\\geo_2006.shp,Status,-1,-1;Score \"Score\" true true false 4 Short 0 4 ,First,#,E:\\GISWork_2\\Rollins_poximity\\GIS\\Wave 3\\Geocoding points\\geo_2006.shp,Score,-1,-1;Match_type \"Match_type\" true true false 2 Text 0 0 ,First,#,E:\\GISWork_2\\Rollins_poximity\\GIS\\Wave 3\\Geocoding points\\geo_2006.shp,Match_type,-1,-1;Side \"Side\" true true false 1 Text 0 0 ,First,#,E:\\GISWork_2\\Rollins_poximity\\GIS\\Wave 3\\Geocoding points\\geo_2006.shp,Side,-1,-1;Match_addr \"Match_addr\" true true false 103 Text 0 0 ,First,#,E:\\GISWork_2\\Rollins_poximity\\GIS\\Wave 3\\Geocoding points\\geo_2006.shp,Match_addr,-1,-1;ARC_Street \"ARC_Street\" true true false 60 Text 0 0 ,First,#,E:\\GISWork_2\\Rollins_poximity\\GIS\\Wave 3\\Geocoding points\\geo_2006.shp,ARC_Street,-1,-1;ARC_Zone \"ARC_Zone\" true true false 40 Text 0 0 ,First,#,E:\\GISWork_2\\Rollins_poximity\\GIS\\Wave 3\\Geocoding points\\geo_2006.shp,ARC_Zone,-1,-1;ID \"ID\" true true false 16 Double 6 15 ,First,#,E:\\GISWork_2\\Rollins_poximity\\GIS\\Wave 3\\Geocoding points\\geo_2006.shp,ID,-1,-1;Address \"Address\" true true false 254 Text 0 0 ,First,#,E:\\GISWork_2\\Rollins_poximity\\GIS\\Wave 3\\Geocoding points\\geo_2006.shp,Address,-1,-1;City \"City\" true true false 254 Text 0 0 ,First,#,E:\\GISWork_2\\Rollins_poximity\\GIS\\Wave 3\\Geocoding points\\geo_2006.shp,City,-1,-1;State \"State\" true true false 254 Text 0 0 ,First,#,E:\\GISWork_2\\Rollins_poximity\\GIS\\Wave 3\\Geocoding points\\geo_2006.shp,State,-1,-1;Zip5 \"Zip5\" true true false 16 Double 6 15 ,First,#,E:\\GISWork_2\\Rollins_poximity\\GIS\\Wave 3\\Geocoding points\\geo_2006.shp,Zip5,-1,-1;ID_1 \"ID_1\" true true false 16 Double 6 15 ,First,#,E:\\GISWork_2\\Rollins_poximity\\GIS\\Wave 3\\Geocoding points\\geo_2006.shp,ID_1,-1,-1;Address_1 \"Address_1\" true true false 254 Text 0 0 ,First,#,E:\\GISWork_2\\Rollins_poximity\\GIS\\Wave 3\\Geocoding points\\geo_2006.shp,Address_1,-1,-1;Year \"Year\" true true false 16 Double 6 15 ,First,#,E:\\GISWork_2\\Rollins_poximity\\GIS\\Wave 3\\Geocoding points\\geo_2006.shp,Year,-1,-1", "CONTAINS", "", "")
-arcpy.AddField_management(clisters3, "Num_Points", "SHORT", "", "", "", "", "NULLABLE", "NON_REQUIRED", "")
-arcpy.CalculateField_management(clisters3, "Num_Points", "[Join_Cou_1]", "VB", "")
+arcpy.SpatialJoin_analysis(clusterTable, points, clusters3, "JOIN_ONE_TO_ONE", "KEEP_ALL", "Join_Count \"Join_Count\" true true false 0 Long 0 0 ,First,#,C:\\Users\\msisk1\\Documents\\ArcGIS\\Default.gdb\\clusterTable,Join_Count,-1,-1;ObjectID \"ObjectID\" true true false 9 Long 0 9 ,First,#,C:\\Users\\msisk1\\Documents\\ArcGIS\\Default.gdb\\clusterTable,ObjectID,-1,-1;ClusterID \"ClusterID\" true true false 0 Short 0 0 ,First,#,C:\\Users\\msisk1\\Documents\\ArcGIS\\Default.gdb\\clusterTable,ClusterID,-1,-1;Status \"Status\" true true false 1 Text 0 0 ,First,#,E:\\GISWork_2\\Rollins_poximity\\GIS\\Wave 3\\Geocoding points\\geo_2006.shp,Status,-1,-1;Score \"Score\" true true false 4 Short 0 4 ,First,#,E:\\GISWork_2\\Rollins_poximity\\GIS\\Wave 3\\Geocoding points\\geo_2006.shp,Score,-1,-1;Match_type \"Match_type\" true true false 2 Text 0 0 ,First,#,E:\\GISWork_2\\Rollins_poximity\\GIS\\Wave 3\\Geocoding points\\geo_2006.shp,Match_type,-1,-1;Side \"Side\" true true false 1 Text 0 0 ,First,#,E:\\GISWork_2\\Rollins_poximity\\GIS\\Wave 3\\Geocoding points\\geo_2006.shp,Side,-1,-1;Match_addr \"Match_addr\" true true false 103 Text 0 0 ,First,#,E:\\GISWork_2\\Rollins_poximity\\GIS\\Wave 3\\Geocoding points\\geo_2006.shp,Match_addr,-1,-1;ARC_Street \"ARC_Street\" true true false 60 Text 0 0 ,First,#,E:\\GISWork_2\\Rollins_poximity\\GIS\\Wave 3\\Geocoding points\\geo_2006.shp,ARC_Street,-1,-1;ARC_Zone \"ARC_Zone\" true true false 40 Text 0 0 ,First,#,E:\\GISWork_2\\Rollins_poximity\\GIS\\Wave 3\\Geocoding points\\geo_2006.shp,ARC_Zone,-1,-1;ID \"ID\" true true false 16 Double 6 15 ,First,#,E:\\GISWork_2\\Rollins_poximity\\GIS\\Wave 3\\Geocoding points\\geo_2006.shp,ID,-1,-1;Address \"Address\" true true false 254 Text 0 0 ,First,#,E:\\GISWork_2\\Rollins_poximity\\GIS\\Wave 3\\Geocoding points\\geo_2006.shp,Address,-1,-1;City \"City\" true true false 254 Text 0 0 ,First,#,E:\\GISWork_2\\Rollins_poximity\\GIS\\Wave 3\\Geocoding points\\geo_2006.shp,City,-1,-1;State \"State\" true true false 254 Text 0 0 ,First,#,E:\\GISWork_2\\Rollins_poximity\\GIS\\Wave 3\\Geocoding points\\geo_2006.shp,State,-1,-1;Zip5 \"Zip5\" true true false 16 Double 6 15 ,First,#,E:\\GISWork_2\\Rollins_poximity\\GIS\\Wave 3\\Geocoding points\\geo_2006.shp,Zip5,-1,-1;ID_1 \"ID_1\" true true false 16 Double 6 15 ,First,#,E:\\GISWork_2\\Rollins_poximity\\GIS\\Wave 3\\Geocoding points\\geo_2006.shp,ID_1,-1,-1;Address_1 \"Address_1\" true true false 254 Text 0 0 ,First,#,E:\\GISWork_2\\Rollins_poximity\\GIS\\Wave 3\\Geocoding points\\geo_2006.shp,Address_1,-1,-1;Year \"Year\" true true false 16 Double 6 15 ,First,#,E:\\GISWork_2\\Rollins_poximity\\GIS\\Wave 3\\Geocoding points\\geo_2006.shp,Year,-1,-1", "CONTAINS", "", "")
+arcpy.AddField_management(clusters3, "Num_Points", "SHORT", "", "", "", "", "NULLABLE", "NON_REQUIRED", "")
+arcpy.CalculateField_management(clusters3, "Num_Points", "[Join_Cou_1]", "VB", "")
 
-deleteAllFieldsBut(clisters3,  ["FID", "Shape","Join_Count","ClusterID","Num_Points"])
+deleteAllFieldsBut(clusters3,  ["FID", "Shape","Join_Count","ClusterID","Num_Points"])
 
 print "table finished"
 
 #Joining back on
 
-#arcpy.SpatialJoin_analysis(clusterTable, points, clisters3, "JOIN_ONE_TO_ONE", "KEEP_ALL",
-arcpy.SpatialJoin_analysis(points, clisters3, geo_2006_SpatialJoin,"JOIN_ONE_TO_ONE", "KEEP_ALL",)
+#arcpy.SpatialJoin_analysis(clusterTable, points, clusters3, "JOIN_ONE_TO_ONE", "KEEP_ALL",
+arcpy.SpatialJoin_analysis(points, clusters3, geo_2006_SpatialJoin,"JOIN_ONE_TO_ONE", "KEEP_ALL",)
 
 deleteAllFieldsBut(geo_2006_SpatialJoin,  ["FID", "Shape","ID","ClusterID","Num_Points"])
 
